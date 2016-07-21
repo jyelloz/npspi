35b0ca68edb8490b1a64b2b55d50ad0cd5a8650b
Jordan Yelloz <jordan@yelloz.me>
2016-07-21 10:38:17 -0700

    added lookup table for "NZR"-encoded bytes
    This was generated by some lua code.

---
+++ nplut.h
@@ -0,0 +1,17 @@
+#ifndef __NPLUT_H__
+#define __NPLUT_H__
+
+#define NPLUT_RATIO 24
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+const unsigned char *
+nplut_get (unsigned int i);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __NPLUT_H__ */
