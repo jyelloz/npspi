f9f253d2053553e76c351b459e07a672009f6b91
Jordan Yelloz <jordan@yelloz.me>
2016-08-12 13:23:28 -0700

    added neopixel gamma correction table

---
+++ npgammalut.h
@@ -0,0 +1,17 @@
+#ifndef __NPGAMMALUT_H__
+#define __NPGAMMALUT_H__
+
+#include <stdint.h>
+
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+uint8_t
+npgammalut_get (uint8_t const i);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __NPGAMMALUT_H__ */
